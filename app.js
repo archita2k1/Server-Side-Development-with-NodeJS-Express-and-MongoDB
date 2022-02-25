6076649c326ea2c49d8a224f4737e793573d7217
Archita Roy Choudhury <archita2k1@gmail.com>
2022-02-26 01:30:50 +0530

    Express Generator

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let dishRouter = require('./routes/dishRouter');
+let leaderRouter = require('./routes/leaderRouter');
+let promoRouter = require('./routes/promoRouter');
+
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
